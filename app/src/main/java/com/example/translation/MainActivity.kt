a427fb73387eae71113cef43c83dd837ebc40b9c
Galal-20 <ga71387@gmail.com>
2023-07-22 13:45:00 +0300

    Translation

--- app/src/main/java/com/example/translation/MainActivity.kt
+++ app/src/main/java/com/example/translation/MainActivity.kt
@@ -1,76 +1,75 @@
 package com.example.translation
 
-import android.app.ProgressDialog.show
-import android.os.Binder
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.translation.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
-import com.google.mlkit.nl.translate.Translator
 import com.google.mlkit.nl.translate.TranslatorOptions
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
-    lateinit var arrayAdapter: ArrayAdapter<String>
-    private var items= arrayOf("Arabic", "English")
-    private var conditions = DownloadConditions.Builder().requireWifi().build()
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var arrayAdapter: ArrayAdapter<String>
+    private val supportedLanguages = arrayOf(
+        TranslateLanguage.ARABIC,
+        TranslateLanguage.ENGLISH,
+        TranslateLanguage.HINDI,
+        TranslateLanguage.DUTCH,
+        TranslateLanguage.GERMAN,
+        TranslateLanguage.FRENCH,
+        TranslateLanguage.JAPANESE,
+        TranslateLanguage.KOREAN,
+        TranslateLanguage.BENGALI,
+        TranslateLanguage.GREEK,
+        TranslateLanguage.BULGARIAN,
+        TranslateLanguage.ITALIAN,
+        TranslateLanguage.INDONESIAN
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-        arrayAdapter = ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line, items)
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, supportedLanguages)
         binding.languageFrom.setAdapter(arrayAdapter)
         binding.languageTo.setAdapter(arrayAdapter)
 
-
-
         binding.translateButton.setOnClickListener {
-            val options = TranslatorOptions.Builder()
-                .setSourceLanguage(selectFrom())
-                .setTargetLanguage(selectTo())
-                .build()
+            val sourceLanguage = binding.languageFrom.text.toString()
+            val targetLanguage = binding.languageTo.text.toString()
 
-            val translator = Translation.getClient(options)
-            translator.downloadModelIfNeeded(conditions)
-                .addOnSuccessListener {
-                    translator.translate(binding.textFrom.text.toString())
-                        .addOnSuccessListener {
-                            binding.textTo.text = it
-                        }
-                }
-                .addOnFailureListener{
-                    Snackbar.make(binding.textFrom, it.message.toString() ,Snackbar.LENGTH_SHORT).show()
-                }
-        }
-
-    }
-
-    private fun selectFrom(): String {
-        return when(binding.textFrom.text.toString()){
-            ""->TranslateLanguage.ENGLISH
-            "Arabic" -> TranslateLanguage.ARABIC
-            "English" -> TranslateLanguage.ENGLISH
-            else-> TranslateLanguage.ENGLISH
+            if (sourceLanguage.isEmpty() || targetLanguage.isEmpty()) {
+                Snackbar.make(binding.root, "Please select source and target languages.", Snackbar.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            translateText(sourceLanguage, targetLanguage, binding.textFrom.text.toString())
         }
     }
 
-    private fun selectTo(): String {
-        return when(binding.textTo.text.toString()){
-            ""->TranslateLanguage.ARABIC
-            "Arabic" -> TranslateLanguage.ARABIC
-            "English"->TranslateLanguage.ENGLISH
-            else-> TranslateLanguage.ARABIC
-        }
+    private fun translateText(sourceLanguage: String, targetLanguage: String, text: String) {
+        val options = TranslatorOptions.Builder()
+            .setSourceLanguage(sourceLanguage)
+            .setTargetLanguage(targetLanguage)
+            .build()
+
+        val translator = Translation.getClient(options)
+        translator.downloadModelIfNeeded(DownloadConditions.Builder().requireWifi().build())
+            .addOnSuccessListener {
+                translator.translate(text)
+                    .addOnSuccessListener { translatedText ->
+                        binding.textTo.text = translatedText
+                    }
+                    .addOnFailureListener { exception ->
+                        Snackbar.make(binding.root, "Translation failed: ${exception.message}", Snackbar.LENGTH_SHORT).show()
+                    }
+            }
+            .addOnFailureListener { exception ->
+                Snackbar.make(binding.root, "Model download failed: ${exception.message}", Snackbar.LENGTH_SHORT).show()
+            }
     }
-
-
 }
